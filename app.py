a683ff72f06325316a0fb0e080695ca9f4b7d41e
BotCelo <marcelo.aguiar.c@gmail.com>
2021-03-09 21:53:34 -0300

    iterate in chracter data

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import Flask, render_template, redirect, url_for, request
 import json
 import requests
+import jinja2
 app = Flask(__name__)
 
 @app.route('/home', methods=['GET', 'POST'])
@@ -17,12 +18,14 @@ def home():
         char = request.form['text']
         char = f"https://api.tibiadata.com/v2/characters/{char}.json"
         char = requests.get(char)
-        
-        char_content = char.content
+        char_content = char.text
+        print((char_content))
+        #loaded_r = json.loads(char_content)
+        #my_json = char_content.decode('utf8').replace("'", '"')
         char = json.loads(char_content)
-        print(char)
-        #render_template('hello.html', char = char)
-        return char
+        #jsonify(char)
+        return render_template('hello.html', chars = char)
+        #return char
 
 @app.route('/registrar')
 def registrar_celo():
